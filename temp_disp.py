436735fcd6b0707534989591dc200f289e508d8f
kyleg740 <2025Gehman.Kyle@fcboe.org>
2025-01-08 13:44:56 -0500

    Create temp_disp.py

---
+++ temp_disp.py
@@ -0,0 +1,73 @@
+import board
+import terminalio
+import displayio
+import time
+from analogio import AnalogIn
+import pwmio
+
+pot = AnalogIn(board.A0)
+y = 65535
+x = 0
+    
+try:
+    from fourwire import FourWire
+except ImportError:
+    from displayio import FourWire
+from adafruit_display_text import label
+from adafruit_st7789 import ST7789
+
+BORDER = 20
+FONTSCALE = 2
+BACKGROUND_COLOR = 0x00FF00  # Bright Green
+FOREGROUND_COLOR = 0xAA0088  # Purple
+TEXT_COLOR = 0xFFFF00
+
+displayio.release_displays()
+
+spi = board.SPI()
+tft_cs = board.D2
+tft_dc = board.D3
+
+display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
+display = ST7789(
+    display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
+)
+
+splash = displayio.Group()
+display.root_group = splash
+
+color_bitmap = displayio.Bitmap(display.width, display.height, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = BACKGROUND_COLOR
+
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+splash.append(bg_sprite)
+
+inner_bitmap = displayio.Bitmap(
+    display.width - BORDER * 2, display.height - BORDER * 2, 1
+)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = FOREGROUND_COLOR
+inner_sprite = displayio.TileGrid(
+    inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
+)
+splash.append(inner_sprite)
+
+while True:
+    p = pot.value
+    a = (p/y) * 3.3
+    x = (a-0.5) * 100
+    b = x * (1.8) + 32.0
+    r = round(b, 2)
+    print(r)
+    time.sleep(0.3)
+    text = str(r) + " Deg F "
+    text_area = label.Label(terminalio.FONT, text=text, color=TEXT_COLOR)
+    text_width = text_area.bounding_box[2] * FONTSCALE
+    text_group = displayio.Group(
+        scale=FONTSCALE,
+        x=display.width // 2 - text_width // 2,
+        y=display.height // 2,
+    )
+    text_group.append(text_area)  
+    splash.append(text_group)
